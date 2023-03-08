a8c0e779861e7028feb02ab820add978d4eba1e9
al3sha9 <optimist.alishan@gmail.com>
2023-03-08 12:35:53 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      
+      <h1>To new beginnings</h1>
+    </div>
+  )
+}
+
+export default App
