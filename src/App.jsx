726bf073fb6173c85e6ed52f95dea4a7701263a1
al3sha9 <optimist.alishan@gmail.com>
2023-03-08 12:45:39 +0500

    Added animated cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import AnimatedCursor from "react-animated-cursor";
 import './App.css'
 
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <div className="App">
-      
+      <div className="cursor__dot">
+        <AnimatedCursor
+          innerSize={15}
+          outerSize={15}
+          color="255, 255 ,255"
+          outerAlpha={0.4}
+          innerScale={0.7}
+          outerScale={5}
+        />
+      </div>
       <h1>To new beginnings</h1>
     </div>
   )
