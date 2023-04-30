6892cecf65532680e5189593d0a0dd8baa09000b
Ali Shan <optimist.alishan@gmail.com>
2023-04-30 23:10:44 +0500

    commit

--- src/components/hero.js
+++ src/components/hero.js
@@ -40,7 +40,7 @@ export default function Hero() {
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z" />
                                         </svg>
                                     </div>
-                                    <div class="col-span-2 pt-1.5">Download CV or RESUME MAIN</div>
+                                    <div class="col-span-2 pt-1.5">Download CV or RESUME KITTY</div>
                                 </div>
                             </a>
                         </div>
